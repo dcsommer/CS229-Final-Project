9fafdb1861b7740c41fbc2b739fa78f237f7eb8c
Daniel Sommermann <dcsommer@gmail.com>
2011-12-10 23:05:12 -0800

    Added extract features and Snip scripts

---
+++ extract_features.m
@@ -0,0 +1,14 @@
+function [features] = extract_features(data, peaks)
+  left = 21;
+  right = 8;
+  numChannels = size(data, 1);
+  numPeaks = size(peaks, 2);
+  featureDim = numChannels * (left + right + 1);
+  features = zeros(numPeaks-2, featureDim);
+  for i=2:(numPeaks-1)
+    leftBound = peaks(i)-left;
+    rightBound = peaks(i)+right;
+    event = data(:, leftBound:rightBound);
+    features(i,:) = reshape(event, numChannels * (rightBound - leftBound + 1), 1);
+  end
+end
