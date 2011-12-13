973226eac84d5399d622c222b4dd5e450991c3cf
David Brody <dbrody@stanford.edu>
2011-12-12 20:09:52 -0800

    Add normalized mean and stdev on runall.m

--- runall.m
+++ runall.m
@@ -3,18 +3,22 @@ load 110810a_1m.mat;
 REDUCED_DIMENSION = 300;
 
 % convert from cells array to matrix
-dataM = zeros(size(data, 1), size(data{1},1));
-for i = 1:size(dataM, 1)
-  dataM(i,:) = data{1} .';
-end
+CHAN_N = length(data);
+CHAN_L = length(data{1});
+
+dataM = (reshape(cell2mat(data),CHAN_L,CHAN_N));
+dataNorm = (dataM - repmat(mean(dataM),CHAN_L,1)) ./ repmat(std(dataM),CHAN_L,1);
 
 % get peaks
-[Data, PeakIndex, PeakAmp, PeakChannel] = Snip(dataM);
+'Get Peaks'
+[Data, PeakIndex, PeakAmp, PeakChannel] = Snip(dataNorm);
 
 % extract features
+'Extract Features'
 [features] = extract_features(Data, PeakIndex);
 
 % reduce dimensions of features via PCA
+'PCA Reduce'
 [reduced_features] = pca_reduce(features, REDUCED_DIMENSION);
 %% reduce dimensions of featuers by fitting a polynomial
 %reduced_features = polyfit(features, REDUCED_DIMENSION);
