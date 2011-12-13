ed4f3ded5059f0cb4cf4882638bf046c2e5c4709
Daniel Sommermann <dcsommer@gmail.com>
2011-12-12 20:58:40 -0800

    Worked on polyfit

--- polyfit_reduce.m
+++ polyfit_reduce.m
@@ -1,5 +1,10 @@
-[reduced_features] = polyfit_reduce(input_features, dims)
-reduced_features = zeros(size(input_features,1), dims);
-for i=1:size(input_features,1)
-  reduced_features(i,:) = polyfit(input_features(i,:), dims);
+% 'dims' is the degree of each polynomial we fit, so the total end
+% result is a (n+1)*60 dimension training example
+[reduced_features] = polyfit_reduce(input, dims)
+reduced_features = zeros(size(input,1), dims);
+for i=1:size(input,1)
+  for j=0:(size(input,2)/30 - 1)
+    reduced_features(i,(j*dim + 1):((j+1)*dim) =
+      polyfit(input(i,((j*30 + 1):((j+1)*30))), dims);
+  end
 end
