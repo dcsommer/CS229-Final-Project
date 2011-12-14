5751507c0eb47ef071b0caa9bda8ff7862520637
David William Brody <dbrody@corn19.Stanford.EDU>
2011-12-13 17:43:30 -0800

    Fix to PCA

--- pca_reduce.m
+++ pca_reduce.m
@@ -3,5 +3,6 @@ function [reduced_features, topDims] = pca_reduce(features, dims)
 
 % Project each feature onto the top 'dims' principal components to reduce
 % the dimension of the features
-topDims = coeff(1:dims, 1:size(coeff,2));
-reduced_features = features * topDims';
+%topDims = coeff(1:dims, 1:size(coeff,2));
+topDims = coeff(:,1:dims);
+reduced_features = features * topDims;
